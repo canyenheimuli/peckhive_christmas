ccfb13801d2eb6527d1ad6d83bcddc3958d75f4f
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-09-10 09:30:46 -0700

    Mobile view dynamic re-sizing setting attempt #15

--- streamlit/app.py
+++ streamlit/app.py
@@ -80,7 +80,7 @@ st.title("Peckhive Christmas Gifting Schedule")
 
 st.markdown("""
 <div style="
-    font-size:  clamp(8px, 1.5vw, 32px);
+    font-size:  clamp(10px, 1.5vw, 32px);
     text-align: center;
     width:      100%;
 ">
